16bddc3bb27e33342445860cf6d8d26389d196a6
ionutmilica <ionut.milica@gmail.com>
2015-05-21 15:44:37 +0300

    Changed the structure

---
+++ app/controllers/account_controller.rb
@@ -0,0 +1,23 @@
+class AccountController < ApplicationController
+  before_action :authenticate_user!
+
+  def wall
+    
+  end
+
+  def questions
+
+  end
+
+  def follow
+
+  end
+
+  def search
+
+  end
+
+  def settings
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,35 +2,14 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  scope '/api' do
-    scope '/v1' do
-      # Token routes
-      scope '/token' do
-        as :user do
-          post 'create', to: 'sessions#create'
-          delete 'destroy', to: 'sessions#destroy'
-        end
-      end
-      scope '/users' do
-        as :user do
-          post 'create', to: 'registrations#create'
-          put 'update', to: 'registrations#update'
-          # Pages
-          get ':id/questions', to: 'users#questions'
-          post ':id/questions', to: 'users#post_question'
-        end
-      end
+  scope 'account' do
+    get 'wall', to: 'account#wall', name: 'account_wall_path'
+    get 'questions', to: 'account#questions', name: 'account_questions_path'
+    get 'follow', to: 'account#follow', name: 'account_follow_path'
+    get 'search', to: 'account#search', name: 'account_search_path'
+    get 'settings', to: 'account#settings', name: 'account_settings_path'
 
-      scope '/user' do
-        get 'questions/unanswered', to: 'users#unanswered_questions'
-        post 'questions/:id/reply', to: 'users#reply_question'
-      end
-
-      scope '/questions' do
-        get ':id', to: 'questions#get_question'
-      end
-      #
-    end
   end
+
   root 'home#index'
 end
